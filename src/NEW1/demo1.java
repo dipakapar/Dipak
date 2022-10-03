f4da27ba18cbc64170c9221ebf30fdbe1728d100
dipak Apar <dipak Apar@Dipak>
2022-10-03 13:29:13 +0530

    First Commit

---
+++ src/NEW1/demo1.java
@@ -0,0 +1,22 @@
+package NEW1;
+
+public class demo1 
+{
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Mesthod is running");
+		
+		TC1();
+		
+		
+	}
+	
+	public static void TC1() 
+	{
+		
+		System.out.println("Hello");
+		
+	}
+
+}
